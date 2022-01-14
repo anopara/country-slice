0d6e184ef9e5622a4d2d6cc3d0a9a0a52fd5f44b
Anastasia Opara <anastasiaopara@gmail.com>
2022-01-14 09:01:20 +0100

    clean

--- src/systems/erase_curve.rs
+++ src/systems/erase_curve.rs
@@ -54,28 +54,6 @@ pub fn erase_curve(
             if !pts_to_delete.is_empty() {
                 stuff.push((i, pts_to_delete, ent.clone()))
             }
-
-            /*
-            let c: Vec<Vec3> = curve
-                .points
-                .iter()
-                // Go through all the points
-                .filter_map(|pt| {
-                    // Check if within distance
-                    if cursor_ws.distance(*pt) > ERASE_BRUSH_SIZE {
-                        Some(*pt)
-                    } else {
-                        // if so delete this point
-                        None
-                    }
-                })
-                .collect();
-
-
-            if c.len() != curve.points.len() {
-                curves_to_replace.push((i, c, ent.clone()));
-            }
-            */
         }
     }
 
@@ -140,62 +118,5 @@ pub fn erase_curve(
                 }
             }
         }
-
-        /*
-        const DIST_THRESHOLD: f32 = crate::resources::wall_manager::RESAMPLING * 2.0;
-        for (i, c, ent) in curves_to_replace {
-            // go through every curve, and see if the dist between a point >, then its a new curve and we need to split
-            let mut splits = Vec::new();
-            for (i, pt) in c.iter().enumerate() {
-                if let Some(next) = c.get(i + 1) {
-                    dbg!(pt.distance(*next));
-                    if pt.distance(*next) > DIST_THRESHOLD {
-                        splits.push(i);
-                    }
-                }
-            }
-            dbg!(c.len());
-            dbg!(splits.clone());
-            // split
-            let mut last_split_index = 0;
-            let mut new_curves = Vec::new();
-            let mut tail = c;
-            for s in splits {
-                let new = tail.split_off(s - last_split_index);
-
-                new_curves.push(tail);
-
-                tail = new;
-
-                last_split_index = s;
-            }
-            new_curves.push(tail);
-            let bla: Vec<_> = new_curves.iter().map(|c| c.len()).collect();
-            dbg!(bla);
-
-            //panic!();
-
-            // check if no degenerate curves
-            new_curves = new_curves
-                .iter()
-                .filter_map(|n| if n.len() > 0 { Some(n.clone()) } else { None })
-                .collect();
-
-            // Update curves
-            for j in 0..new_curves.len() {
-                if j == 0 {
-                    wall_manager.curves[i] = (Curve::from(new_curves[0].clone()), ent);
-                    ev_curve_changed.send(CurveChangedEvent { curve_index: i });
-                } else {
-                    wall_manager
-                        .curves
-                        .push((Curve::from(new_curves[j].clone()), None));
-                    ev_curve_changed.send(CurveChangedEvent {
-                        curve_index: wall_manager.curves.len() - 1,
-                    });
-                }
-            }
-        }
-        */
     }
 }
