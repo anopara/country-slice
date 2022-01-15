2a71e1a4b7ca0466837942149bb50d961e1297ac
Anastasia Opara <anastasiaopara@gmail.com>
2022-01-15 13:37:29 +0100

    thank you kitty

--- src/systems/render/ssbo_delete.rs
+++ src/systems/render/ssbo_delete.rs
@@ -1,8 +1,7 @@
 use crate::render::ssbo::SSBO_TO_DELETE;
 
 pub fn delete_dropped_ssbos() {
-    let bla = std::mem::replace(&mut *SSBO_TO_DELETE.lock().unwrap(), Vec::new());
-    for binding_point in bla {
+    for binding_point in SSBO_TO_DELETE.lock().unwrap().drain(..) {
         unsafe {
             gl::DeleteBuffers(1, &binding_point);
         }
