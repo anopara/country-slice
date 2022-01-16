7bb2c1c3dc63d86df30a2e971ad5e34b5bdbb274
Anastasia Opara <anastasiaopara@gmail.com>
2022-01-16 08:31:32 +0100

    cleanup

--- src/systems/eraser.rs
+++ src/systems/eraser.rs
@@ -79,7 +79,6 @@ pub fn eraser(
         }
 
         // if no curves left, send an evene to delete this curve completely
-        // TODO: clear memory of lefotver VAOs
         if cc.is_empty() {
             ev_curve_deleted.send(CurveDeletedEvent {
                 curve_index: *curve_index,
@@ -103,95 +102,4 @@ pub fn eraser(
             }
         }
     }
-
-    /*
-    // Go through all the curves
-    //let mut curves_to_replace = Vec::new();
-    let mut stuff = Vec::new();
-    {
-        puffin::profile_scope!("find deletions");
-        for (i, (curve, ent)) in wall_manager.curves.iter().enumerate() {
-            let pts_to_delete: Vec<usize> = curve
-                .points
-                .iter()
-                .enumerate()
-                .filter_map(|(j, pt)| {
-                    if cursor_ws.distance(*pt) < ERASE_BRUSH_SIZE {
-                        Some(j)
-                    } else {
-                        // if so delete this point
-                        None
-                    }
-                })
-                .collect();
-
-            if !pts_to_delete.is_empty() {
-                stuff.push((i, pts_to_delete, ent.clone()))
-            }
-        }
-    }
-
-    {
-        puffin::profile_scope!("split");
-
-        for (curve_indx, pts_to_delete, ent) in stuff.iter() {
-            //dbg!(curve_indx);
-            //dbg!(pts_to_delete);
-            //panic!();
-
-            let curve = &wall_manager.curves[*curve_indx].0;
-
-            let mut splits = vec![(0, pts_to_delete[0])];
-            for (i, p_index) in pts_to_delete.iter().enumerate() {
-                if let Some(next) = pts_to_delete.get(i + 1) {
-                    if p_index + 1 != *next {
-                        // that's a break!
-                        splits.push((p_index + 1, *next));
-                    }
-                } else {
-                    splits.push((p_index + 1, curve.points.len()));
-                }
-            }
-
-            //(TODO: actually delete the lingering points!) otherwise we have these leftovers of 2 segments everywhere
-            // those are probably lingering SSBOs and VAOs that are never properly cleared out
-            // need to have a way to call a full curve deletion
-
-            // check if no degenerate curves
-            splits = splits
-                .iter()
-                .filter_map(move |(start, end)| {
-                    if end - start > 2 {
-                        Some((*start, *end))
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-
-            //dbg!(splits);
-            let new_curves: Vec<Vec<Vec3>> = splits
-                .iter()
-                .map(|(start, end)| curve.points[*start..*end].to_vec())
-                .collect();
-
-            // Update curves
-            for j in 0..new_curves.len() {
-                if j == 0 {
-                    wall_manager.curves[*curve_indx] = (Curve::from(new_curves[0].clone()), *ent);
-                    ev_curve_changed.send(CurveChangedEvent {
-                        curve_index: *curve_indx,
-                    });
-                } else {
-                    wall_manager
-                        .curves
-                        .push((Curve::from(new_curves[j].clone()), None));
-                    ev_curve_changed.send(CurveChangedEvent {
-                        curve_index: wall_manager.curves.len() - 1,
-                    });
-                }
-            }
-        }
-    }
-    */
 }
